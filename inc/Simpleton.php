039a9138f0230fcbf3be7788d06c9b64c2b40b32
Dmitry Pripa <i@dpripa.com>
2022-11-28 15:05:20 +0200

    fix simpleton

--- inc/Simpleton.php
+++ inc/Simpleton.php
@@ -5,15 +5,15 @@ namespace MyPlugin;
 defined('ABSPATH') || exit;
 
 trait Simpleton {
-	private static $is_initialized = false;
+	protected static $is_initialized = false;
 
-	private function is_initialized(): bool {
-		if (self::$is_initialized) {
+	protected function is_initialized(): bool {
+		if (static::$is_initialized) {
 			throw new \Exception('Can only be initialized once');
 		}
 
-		$is_initialized = self::$is_initialized;
-		self::$is_initialized = true;
+		$is_initialized = static::$is_initialized;
+		static::$is_initialized = true;
 
 		return $is_initialized;
 	}
