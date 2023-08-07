5ac1a83c7e57cc520419df26c24ba57477bc3290
Dmitry Pripa <i@dpripa.com>
2023-08-07 17:33:00 +0300

    implement Env

--- inc/Core/Extension/Env.php
+++ inc/Core/Extension/Env.php
@@ -8,4 +8,23 @@ defined('ABSPATH') || exit;
 
 class Env {
 	use Singleton;
+
+	protected $root_host;
+	protected $development_hosts = [
+		'localhost',
+		'local',
+		'loc',
+		'development',
+		'dev',
+	];
+
+	protected function __construct() {
+		$url = Url::get_instance();
+		$host = explode('.', wp_parse_url($url->get_home(), PHP_URL_HOST));
+		$this->root_host = end($host);
+	}
+
+	public function is_development(): bool {
+		return in_array($this->root_host, $this->development_hosts, true);
+	}
 }
