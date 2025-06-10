6a49fc49d2c13406e8303f997dea5641b5fede84
Dmitry Pripa <dmitrypripa@gmail.com>
2025-06-10 04:18:09 +0300

    add compatibility with omgcore 1.0.1

--- inc/App.php
+++ inc/App.php
@@ -1,29 +1,21 @@
 <?php
 namespace StarterPlugin;
 
-use StarterPlugin\OmgCore\App as AbstractApp;
+use StarterPlugin\OmgCore\OmgApp;
 
 defined( 'ABSPATH' ) || exit;
 
-class App extends AbstractApp {
+class App extends OmgApp {
 	protected function __construct() {
 		parent::__construct( ROOT_FILE, KEY );
 
-		add_action( 'plugins_loaded', $this->init() );
 		add_action( 'init', $this->load_textdomain() );
+		add_action( 'wp_enqueue_scripts', $this->enqueue_assets() );
 		register_activation_hook( ROOT_FILE, $this->activate() );
 		register_deactivation_hook( ROOT_FILE, $this->deactivate() );
 	}
 
-	protected function init(): callable {
-		return function (): void {
-			if ( $this->requirement->validate() ) {
-				return;
-			}
-
-			add_action( 'wp_enqueue_scripts', $this->enqueue_assets() );
-		};
-	}
+	protected function init(): void {}
 
 	protected function load_textdomain(): callable {
 		return function (): void {
