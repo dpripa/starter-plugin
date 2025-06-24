290b763a40e0e9a72e6a16d5f9407935aa9fda77
Dmitry Pripa <dmitrypripa@gmail.com>
2025-06-24 20:25:26 +0300

    Downgrade omgcore dependency and reorder imports
    Changed omgpress/omgcore version requirement from ^1.0.3 to ^0.1.0 in composer.json and updated composer.lock accordingly. In App.php, reordered Dependency and OmgApp imports and moved get_config() method to the end of the class for improved code organization.

--- inc/App.php
+++ inc/App.php
@@ -1,34 +1,14 @@
 <?php
 namespace StarterPlugin;
 
-use StarterPlugin\OmgCore\Dependency;
 use StarterPlugin\OmgCore\OmgApp;
+use StarterPlugin\OmgCore\Dependency;
 
 defined( 'ABSPATH' ) || exit;
 
 class App extends OmgApp {
 	protected function __construct() {
 		parent::__construct( ROOT_FILE, KEY );
-	}
-
-	protected function get_config(): array {
-		return array(
-			Dependency::class => array(
-				'notice_title_required_singular'         => __( 'The <b>%1$s</b> plugin%2$s is <b>required</b> for the <b>%3$s</b> features to function.', 'starter-plugin' ),
-				'notice_title_optional_singular'         => __( 'The <b>%1$s</b> plugin%2$s is <b>recommended</b> for the all <b>%3$s</b> features to function.', 'starter-plugin' ),
-				'notice_title_required_plural'           => __( 'The following plugins are <b>required</b> for the <b>%s"/b> features to function:', 'starter-plugin' ),
-				'notice_title_optional_plural'           => __( 'The following plugins are <b>recommended</b> for the all <b>%s</b> features to function:', 'starter-plugin' ),
-				'notice_item_not_installed'              => __( 'not installed', 'starter-plugin' ),
-				'notice_item_undefiled_installation_url' => __( 'not installed, can\'t be installed automatically', 'starter-plugin' ),
-				'notice_btn_activate'                    => __( 'Activate', 'starter-plugin' ),
-				'notice_btn_install_and_activate'        => __( 'Install and activate', 'starter-plugin' ),
-				'notice_btn_activate_only_required'      => __( 'Activate only required', 'starter-plugin' ),
-				'notice_btn_install_and_activate_only_required' => __( 'Install and activate only required', 'starter-plugin' ),
-				'notice_success_activate'                => __( 'Required plugin(s) activated.', 'starter-plugin' ),
-				'notice_success_install_and_activate'    => __( 'Required plugin(s) installed and activated.', 'starter-plugin' ),
-				'notice_error_install'                   => __( 'The "%1$s" plugin can\'t be installed automatically. Please install it manually.', 'starter-plugin' ),
-			),
-		);
 	}
 
 	protected function init(): callable {
@@ -55,5 +35,25 @@ class App extends OmgApp {
 		return function (): void {
 			$this->admin_notice->reset();
 		};
+	}
+
+	protected function get_config(): array {
+		return array(
+			Dependency::class => array(
+				'notice_title_required_singular'         => __( 'The <b>%1$s</b> plugin%2$s is <b>required</b> for the <b>%3$s</b> features to function.', 'starter-plugin' ),
+				'notice_title_optional_singular'         => __( 'The <b>%1$s</b> plugin%2$s is <b>recommended</b> for the all <b>%3$s</b> features to function.', 'starter-plugin' ),
+				'notice_title_required_plural'           => __( 'The following plugins are <b>required</b> for the <b>%s"/b> features to function:', 'starter-plugin' ),
+				'notice_title_optional_plural'           => __( 'The following plugins are <b>recommended</b> for the all <b>%s</b> features to function:', 'starter-plugin' ),
+				'notice_item_not_installed'              => __( 'not installed', 'starter-plugin' ),
+				'notice_item_undefiled_installation_url' => __( 'not installed, can\'t be installed automatically', 'starter-plugin' ),
+				'notice_btn_activate'                    => __( 'Activate', 'starter-plugin' ),
+				'notice_btn_install_and_activate'        => __( 'Install and activate', 'starter-plugin' ),
+				'notice_btn_activate_only_required'      => __( 'Activate only required', 'starter-plugin' ),
+				'notice_btn_install_and_activate_only_required' => __( 'Install and activate only required', 'starter-plugin' ),
+				'notice_success_activate'                => __( 'Required plugin(s) activated.', 'starter-plugin' ),
+				'notice_success_install_and_activate'    => __( 'Required plugin(s) installed and activated.', 'starter-plugin' ),
+				'notice_error_install'                   => __( 'The "%1$s" plugin can\'t be installed automatically. Please install it manually.', 'starter-plugin' ),
+			),
+		);
 	}
 }
